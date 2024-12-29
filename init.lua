f9d4dc6509a2e3790e2cdb7f145d3ce349ce0d4f
TomCash008 <tomcash008@gmail.com>
2024-12-29 10:52:49 +0800

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,24 @@
+--名称
+appname="LocalMusicPlayer"
+--包名
+packagename="com.tomcash.localmusicplayer"
+--版本
+appver="1.0"
+--版本号
+appcode="1099"
+--SDK
+appsdk="29"
+--调试模式
+debugmode=false
+--调试面板模式(1/2/3)
+debuggermode=1
+--应用权限
+user_permission={
+  "INTERNET",
+  "READ_EXTERNAL_STORAGE",
+  "WRITE_EXTERNAL_STORAGE"
+}
+--跳过编译
+skip_compilation={
+  
+}
